7771fffd437467aa9aa13faa2fda061bf4938b13
dongseok <dskang207@naver.com>
2023-08-20 02:29:26 +0900

    sixth upload

--- lib/riview/riview_test.dart
+++ lib/riview/riview_test.dart
@@ -4,19 +4,24 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'data.dart';
+import '../model/model_riview.dart';
 
-Future<Album> fetchAlbum() async {
+Future<List<model_riview>> fetchRiview() async {
+  int i=1;
   final response = await http
-      .get(Uri.parse('http://ajsv.dothome.co.kr/test/restapiPage.php'));
+      .get(Uri.parse('http://ajsv.dothome.co.kr/test/restapiPage.php?value='+'${i}'));
 
   if (response.statusCode == 200) {
-    // If the server did return a 200 OK response, then parse the JSON.
-    // 만약 서버가 ok 응답을 반환하면, JSON을 파싱합니다.
-    return Album.fromJson(jsonDecode(response.body));
+
+    var responseBody = utf8.decode(response.bodyBytes);
+    final json = "[${responseBody}]";  //responseBody는 {키:값},{키:값}형태
+    List list = (jsonDecode(json)as List<dynamic>);
+    return list.map<model_riview>((map) => model_riview.fromJson(map)).toList();
+
+    //return Post.fromJson(jsonDecode(response.body));
   } else {
-    // If the server did not return a 200 OK response, then throw an exception.
-    // 만약 응답이 ok가 아니라면 오류를 출력
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
     throw Exception('Failed to load album');
   }
 }
@@ -28,30 +33,35 @@ class riview_test extends StatefulWidget {
 }
 
 class _ListTileAppState extends State<riview_test> {
-  late Future<Album> futureAlbum; //late : non-nullable 변수의 초기화를 나중에 할 수 있게해줌
+  late Future<List<model_riview>> futureriview; //late : non-nullable 변수의 초기화를 나중에 할 수 있게해줌
 
   @override
   void initState(){
     super.initState();
-    futureAlbum=fetchAlbum();//futureAlbum이 빌드시 fetchAlbum()으로 초기화 됨
+    futureriview=fetchRiview();//futureAlbum이 빌드시 fetchAlbum()으로 초기화 됨
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child:FutureBuilder<Album>(
-          future: futureAlbum,//작업하고하자는 future -> fetchAlbum()의 리턴값
+        child:FutureBuilder(
+          future: futureriview,//작업하고하자는 future -> fetchAlbum()의 리턴값
           builder: (context,snapshot){ //snapshot 정보를 통해 비동기 처리 결과를 어들 수 있음
             if (snapshot.hasData) {
+              List<model_riview> _riview = snapshot.data as List<model_riview>;
               return Center(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(snapshot.data!.http_status),
-                    Text(snapshot.data!.data1),
-                    Text(snapshot.data!.data2),
-                    Text(snapshot.data!.data3[2]),
+                    Text(_riview[0].http_status),
+                    Text(_riview[0].loc_code),
+                    Text(_riview[0].name),
+                    Text(_riview[0].regDate),
+                    Text(_riview[0].rating.toString()),
+                    Text(_riview[0].content),
+                    Text(_riview[0].imglink),
+
                   ],
                 ),
               );
